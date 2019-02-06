9d79337769fd069aaf639016c6391a8d8375f622
kalpana16cs045 <47378390+kalpana16cs045@users.noreply.github.com>
2019-02-06 12:13:20 +0530

    Update java01.java

--- java01.java
+++ java01.java
@@ -2,9 +2,9 @@
 {
     public static void main(String[] arg)
     int number=3;
-    if (number<0.0)
+    if (number>0)
     System.out.println("Postive");
-    else if(number<0.0)
+    else if(number<0)
     System. out.println("Negative");
     else
     system. out.println(number+"is 0");
