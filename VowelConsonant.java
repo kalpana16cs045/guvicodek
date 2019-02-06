1047f0c1c73348b33946f0796630cec3b8436792
kalpana16cs045 <47378390+kalpana16cs045@users.noreply.github.com>
2019-02-06 15:13:50 +0530

    Create VowelConsonant.java

---
+++ VowelConsonant.java
@@ -0,0 +1,17 @@
+class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'e';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) 
+        {
+            System.out.println(ch + " the is vowel");
+        }
+        else
+        {
+            System.out.println(ch + " the is consonant");
+        }
+
+    }
+}
